1fb1346d394433563b4fa703fda66472c824feb9
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-12-30 22:26:37 +0100

    0.0.1

---
+++ src/build.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+#
+# Build script
+#
+###########################################################
+
+DOCKER_LOG=/var/log/docker.log
+DEBUG_LOG=/dev/null
+if [ "$DEBUG" = true ]; then
+  DEBUG_LOG=$DOCKER_LOG
+fi
+
+if ! [ -z "$DEVICE_LIST" ]; then
+
+  # cd to working directory
+  cd $SRC_DIR
+
+  # If the source directory is empty
+  if ! [ "$(ls -A $SRC_DIR)" ]; then
+    # Initialize repository
+    echo ">> [$(date)] Initializing repository" >> $DOCKER_LOG
+    yes | repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_lineageos.git -b $BRANCH_NAME 2>&1 >&$DEBUG_LOG
+  fi
+
+  # Copy local manifests to the appropriate folder in order take them into consideration
+  echo ">> [$(date)] Copying '$LMANIFEST_DIR/*.xml' to '$SRC_DIR/.repo/local_manifests/'" >> $DOCKER_LOG
+  mkdir -p $SRC_DIR/.repo/local_manifests
+  cp $LMANIFEST_DIR/*.xml $SRC_DIR/.repo/local_manifests/ >&$DEBUG_LOG
+
+  # Go to "vendor/cm" and reset it's current git status ( remove previous changes ) only if the directory exists
+  if [ -d "vendor/cm" ]; then
+    cd vendor/cm
+    git reset --hard 2>&1 >&$DEBUG_LOG
+    cd $SRC_DIR
+  fi
+
+  # Sync the source code
+  echo ">> [$(date)] Syncing repository" >> $DOCKER_LOG
+  repo sync 2>&1 >&$DEBUG_LOG
+
+  # If requested, clean the OUT dir in order to avoid clutter
+  if [ "$CLEAN_OUTDIR" = true ]; then
+    echo ">> [$(date)] Cleaning '$IMG_DIR'" >> $DOCKER_LOG
+    cd $IMG_DIR
+    rm *
+    cd $SRC_DIR
+  fi
+
+  # Prepare the environment
+  echo ">> [$(date)] Preparing build environment" >> $DOCKER_LOG
+  source build/envsetup.sh 2>&1 >&$DEBUG_LOG
+
+  # Set a custom updater URI if a OTA URL is provided
+  if ! [ -z "$OTA_URL" ]; then
+    echo ">> [$(date)] Adding OTA URL '$OTA_URL' to build.prop" >> $DOCKER_LOG
+    sed -i "1s;^;PRODUCT_PROPERTY_OVERRIDES += $OTA_PROP=$OTA_URL\n\n;" vendor/cm/config/common.mk >&$DEBUG_LOG
+  fi
+
+  # Cycle DEVICE_LIST environment variable, to know which one may be executed next
+  IFS=','
+  for codename in $DEVICE_LIST; do
+    if ! [ -z "$codename" ]; then
+      # Start the build
+      echo ">> [$(date)] Starting build for $codename" >> $DOCKER_LOG
+      breakfast $codename 2>&1 >&$DEBUG_LOG
+      if mka recoveryimage 2>&1 >&$DEBUG_LOG; then
+        # Move produced IMG files to the main OUT directory
+        echo ">> [$(date)] Moving build artifacts for $codename to '$IMG_DIR'" >> $DOCKER_LOG
+        cd $SRC_DIR
+        find out/target/product/$codename -name '*twrp*.img*' -exec mv {} $IMG_DIR \; >&$DEBUG_LOG
+      else
+        echo ">> [$(date)] Failed build for $codename" >> $DOCKER_LOG
+      fi
+      # Clean everything, in order to start fresh on next build
+      if [ "$CLEAN_AFTER_BUILD" = true ]; then
+        echo ">> [$(date)] Cleaning build for $codename" >> $DOCKER_LOG
+        make clean 2>&1 >&$DEBUG_LOG
+      fi
+      echo ">> [$(date)] Finishing build for $codename" >> $DOCKER_LOG
+    fi
+  done
+
+  # Clean the src directory if requested
+  if [ "$CLEAN_SRCDIR" = true ]; then
+    rm -Rf "$SRC_DIR/*"
+  fi
+fi
---
+++ src/init.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Init script
+#
+###########################################################
+
+DOCKER_LOG=/var/log/docker.log
+DEBUG_LOG=/dev/null
+if [ "$DEBUG" = true ]; then
+  DEBUG_LOG=$DOCKER_LOG
+fi
+
+# Initialize CCache if it will be used
+if [ "$USE_CCACHE" = 1 ]; then
+  ccache -M $CCACHE_SIZE 2>&1 >&$DEBUG_LOG
+fi
+
+# Initialize Git user information
+git config --global user.name $USER_NAME
+git config --global user.email $USER_MAIL
+
+# Initialize the cronjob
+cronFile=/tmp/buildcron
+printf "SHELL=/bin/bash\n" > $cronFile
+printenv -0 | sed -e 's/=\x0/=""\n/g'  | sed -e 's/\x0/\n/g' | sed -e "s/_=/PRINTENV=/g" >> $cronFile
+printf "\n$CRONTAB_TIME /usr/bin/flock -n /tmp/lock.build /root/build.sh >> $DOCKER_LOG 2>&1\n" >> $cronFile
+crontab $cronFile
+rm $cronFile
+
+# Run crond in foreground
+cron -f 2>&1
